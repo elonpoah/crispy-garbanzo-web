6ef5022bff460b077932e46479b9807e01841eb0
lcopo4247 <lcopo4247@gmail.com>
2024-08-11 08:12:47 +0800

    use vant4 and setting auto-import

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { VantResolver } from '@vant/auto-import-resolver';
 import postCssPxToRem from 'postcss-pxtorem'
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
 import path from 'path';
@@ -10,6 +13,12 @@ import path from 'path';
 export default defineConfig({
   plugins: [
     vue(),
+    AutoImport({
+      resolvers: [VantResolver()],
+    }),
+    Components({
+      resolvers: [VantResolver()],
+    }),
     createSvgIconsPlugin({
       iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
       symbolId: 'icon-[name]',
