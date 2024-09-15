91457b478f507406386efd0a8162618b15a87e44
lcopo4247 <lcopo4247@gmail.com>
2024-09-15 14:26:51 +0800

    lucky wheel

--- src/views/FreespinDetail.vue
+++ src/views/FreespinDetail.vue
@@ -2,21 +2,14 @@
   <div class="page">
     <NavBack title="Freespin" />
     <div class="page-inner">
-      <div class="pitem green">
-        <div class="content">
-          <p class="title">{{ descripCur.title }}</p>
-          <p class="bonus">{{ $t('free.upt') }} {{ descripCur.bonus }} {{ $t('free.bnus') }}</p>
-        </div>
-      </div>
-      <div>
-        <button class="button active submit-btn">
-            {{ $t('free.jinnw') }}
-        </button>
+      <div id="my-lucky"></div>
+      <div class="share-link" v-clipboard:copy="shareURL" v-clipboard:success="onSuccess" v-clipboard:error="onError">
+        <p>{{ shareURL }}</p>
+        <svg-icon name="Copy" class="share-icon" />
       </div>
       <div class="content-rule">
-        <h2>{{ $t('session_detail.title') }}</h2>
+        <h2>{{ $t('free.rules') }}</h2>
         <p>1.{{ descripCur.desc1 }} <span class="warning">{{ descripCur.count }}</span> {{ $t('free.join')}}</p>
-        <h2>{{ descripCur.validusers }}</h2>
         <p class="active">2.{{ $t('free.desc3') }}</p>
       </div>
     </div>
@@ -25,49 +18,152 @@
 <script setup lang="ts">
 import { useI18n } from 'vue-i18n';
 import NavBack from "@/components/NavBack.vue";
-import { computed } from "vue";
+import { computed, ref } from "vue";
 import { useRoute } from 'vue-router';
 import { FreeType } from '@/constant/index'
+import LuckyWheel from '@/lucky/lib/wheel';
+import { onMounted } from 'vue';
+import { showNotify, showDialog } from 'vant';
+import bf from '@/assets/images/lucky/bg.png';
+import pr from '@/assets/images/lucky/prize.png';
+import btn from '@/assets/images/lucky/btn.png'
 
 const route = useRoute()
 const { t } = useI18n()
 const type = computed(()=> route.params.type)
 
+const shareURL = ref('http://localhost/freespin/daily')
+const onSuccess = () => {
+  
+  showNotify({ type: 'success', teleport: '#app', message: t('others.copysuccess') });
+}
+const onError = () => {
+  
+  showNotify({ type: 'warning', teleport: '#app', message: t('others.copyfail') });
+}
+
 const descrip = {
   [FreeType.daily]: {
     bonus: 5,
     count: 1,
-    validusers: t('free.validusers'),
     title: t('free.daily.title'),
     desc1: t('free.daily.desc1'),
-    desc3: t('free.daily.desc3')
   },
   [FreeType.weekly]: {
     bonus: 30,
     count: 5,
-    validusers: t('free.validusers'),
     title: t('free.weekly.title'),
     desc1: t('free.weekly.desc1'),
-    desc3: t('free.weekly.desc3')
   },
   [FreeType.monthly]: {
     bonus: 100,
     count: 18,
-    validusers: t('free.validusers'),
     title: t('free.monthly.title'),
     desc1: t('free.monthly.desc1'),
-    desc3: t('free.monthly.desc3')
   }
 }
 const descripCur = computed(()=> descrip[route.params.type as FreeType])
+
+const LuckyCanvasWheel = () => {
+  // @ts-ignore
+  const myLucky = new LuckyWheel('#my-lucky', {
+    width: '375px',
+    height: '375px',
+    blocks: [
+      {
+        padding: '50px',
+        imgs: [{
+          src: bf,
+          width: '100%',
+          height: '100%'
+        }]
+      },
+      {
+        imgs: [{
+          src: pr,
+          width: '100%',
+          height: '100%',
+          rotate: true
+        }]
+      }
+    ],
+    prizes: [
+      { fonts: [{ text: '' }] },
+      { fonts: [{ text: '' }] },
+      { fonts: [{ text: '' }] },
+      { fonts: [{ text: '' }] },
+      { fonts: [{ text: '' }] },
+      { fonts: [{ text: '' }] },
+      { fonts: [{ text: '' }] },
+      { fonts: [{ text: '' }] },
+    ],
+    buttons: [
+      {
+        radius: '45%',
+        imgs: [{
+          src: btn,
+          width: '100%',
+          top: '-100%'
+        }]
+      },
+      {
+        radius: '30%', background: 'transparent',
+        fonts: [{ text: 'SPIN', top: '-10px',fontColor: '#ffffff', fontWeight: '800' }]
+      },
+    ],
+    start: function() {
+      // 开始游戏
+      myLucky.play()
+      // 假设接口的请求速度是1s
+      // @ts-ignore
+      setTimeout(_ => {
+        // 停止游戏
+        myLucky.stop(0)
+      }, 1000)
+    },
+    end: function(prize:any) { // 游戏停止时触发
+      showDialog({
+        message: t('free.congratulations')+prize.fonts[0].text,
+      }).then(() => {
+        
+      });
+    }
+  })
+}
+onMounted(()=> {
+  LuckyCanvasWheel()
+})
 </script>
 <style lang="less" scoped>
+.page {
+  height: 100vh;
+  background: url(/src/assets/images/lucky/bg.png);
+  background-position: center;
+  overflow-y: auto;
+}
 .page-inner {
   min-height: calc(100vh - 55px);
-  padding: 10px;
-  background: linear-gradient(180deg,rgba(23,183,69,.2) 0%,rgba(30,130,59,0) 100%),#1C1E22;
+  padding: 0 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  .share-link {
+    margin: 10px auto;
+    font-size: 12px;
+    padding: 5px 10px;
+    border: 1px solid #44eff4;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .share-icon {
+      fill: #d9dde0;
+      margin-left: 5px;
+    }
+  }
   .content-rule {
     font-size: 14px;
+    margin-top: 20px;
     h2 {
       font-size: 16px;
       color: #d9dde0;
@@ -79,64 +175,6 @@ const descripCur = computed(()=> descrip[route.params.type as FreeType])
       .warning {
         color: red;
       }
-    }
-  }
-  .submit-btn {
-      width: 100%;
-      height: 50px;
-      border-radius: 50px;
-      font-size: 18px;
-      margin-bottom: 20px;
-      text-transform: uppercase;
-      &.is-got {
-        animation: heartbeat 2s infinite linear;
-      }
-    }
-  .pitem {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    margin-bottom: 17px;
-    border-radius: 4px;
-    box-shadow: 0 4px 14px 0 #0b0b0b;
-    .content {
-      padding: 20px 0 20px 10px;
-      .title {
-        font-size: 26px;
-        font-weight: 700;
-        text-transform: uppercase;
-      }
-      .bonus {
-        font-size: 16px;
-        font-weight: 500;
-      }
-      .join {
-        margin-top: 10px;
-        width: 120px;
-        font-size: 14px;
-        font-weight: 500;
-        text-transform: uppercase;
-        &.purple {
-          background: #6d2bff;
-        }
-        &.orange {
-          background: #c46b02;
-        }
-      }
-    }
-    &.green {
-      background: linear-gradient(180deg,rgba(23,183,69,.2) 0%,rgba(30,130,59,0) 100%),#1C1E22;
-    }
-    &.purple {
-      background: linear-gradient(26.79deg, rgba(109, 43, 255, 0.480402) 1.86%, rgba(109, 43, 255, 0.04) 85.25%, rgba(109, 43, 255, 0.04) 85.25%), rgba(216, 216, 216, 0.05);
-    }
-    &.orange {
-      background: linear-gradient(180deg,rgba(196,107,2,.2) 0%,rgba(196,107,2,0) 100%),#1C1E22;
-    }
-    .img {
-      width: 150px;
-      height: 150px;
-      object-fit: cover;
     }
   }
 }
